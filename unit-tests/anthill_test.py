20f2b2d85a73c92d0d45f513ae2b6616cd6d69ab
jsokolowska <joana.sokolowska@gmail.com>
2020-04-14 21:28:07 +0200

    Added unit tests for getting best and worst ant in anthill

---
+++ unit-tests/anthill_test.py
@@ -0,0 +1,25 @@
+import unittest
+from src.anthill import Anthill
+
+
+class anthillTest(unittest.TestCase):
+    def test_get_best_ant(self):
+        anthill = Anthill(10)
+        for i in range(10):
+            anthill.ants[i].distance_traveled = i**2
+
+        best_ant = anthill.get_best_ant()
+        self.assertEqual(best_ant.distance_traveled, 0)
+
+    def test_get_worst_ant(self):
+        anthill = Anthill(10)
+        for i in range(10):
+            anthill.ants[i].distance_traveled = i ** 2
+
+        worst_ant = anthill.get_worst_ant()
+        self.assertEqual(worst_ant.distance_traveled, 81)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
