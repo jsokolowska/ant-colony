3365649e24b1bdd8eddfd67881e723a6d0a5c37a
jsokolowska <joana.sokolowska@gmail.com>
2020-04-19 20:50:22 +0200

    Fixed unit tests for reading graph input and added mockGraph as a comparison object

--- unit-tests/input_test.py
+++ unit-tests/input_test.py
@@ -11,32 +11,29 @@ from src.graph import Graph
 
 
 class TestInputFunctions(unittest.TestCase):
+    def setUp(self):
+        self.mockGraph = Graph()
+        self.mockGraph.add_edge("A", "B", 1)
+        self.mockGraph.add_edge("A", "C", 1)
+        self.mockGraph.add_edge("A", "D", 8)
+        self.mockGraph.add_edge("B", "D", 3)
+        self.mockGraph.add_edge("C", "D", 1)
+        self.mockGraph.start = "A"
+        self.mockGraph.end = "D"
 
     def test_read_txt(self):
         graph1 = read_graph_txt(keyword_1="test", keyword_2="graph")
-        graph2 = Graph()
-        graph2.add_edge("A", "B", 1)
-        graph2.add_edge("A", "C", 1)
-        graph2.add_edge("B", "D", 9)
-        graph2.add_edge("C", "D", 1)
         lst = ["A", "B", "C", "D"]
-        graph2.start = "A"
-        graph2.start = "D"
-        self.assertListEqual(list(graph1.vertices.keys()), list(graph2.vertices.keys()))
+        self.assertListEqual(list(graph1.vertices.keys()), list(self.mockGraph.vertices.keys()))
         for i in range(4):
-            self.assertDictEqual(graph1.vertices[lst[i]].neighbours, graph2.vertices[lst[i]].neighbours)
+            self.assertDictEqual(graph1.vertices[lst[i]].neighbours, self.mockGraph.vertices[lst[i]].neighbours)
 
     def test_read_from_file(self):
         graph1 = read_graph_from_file("../graphs/test_graph.txt")
-        graph2 = Graph()
-        graph2.add_edge("A", "B", 1)
-        graph2.add_edge("A", "C", 1)
-        graph2.add_edge("B", "D", 9)
-        graph2.add_edge("C", "D", 1)
         lst = ["A", "B", "C", "D"]
-        self.assertListEqual(list(graph1.vertices.keys()), list(graph2.vertices.keys()))
+        self.assertListEqual(list(graph1.vertices.keys()), list(self.mockGraph.vertices.keys()))
         for i in range(4):
-            self.assertDictEqual(graph1.vertices[lst[i]].neighbours, graph2.vertices[lst[i]].neighbours)
+            self.assertDictEqual(graph1.vertices[lst[i]].neighbours, self.mockGraph.vertices[lst[i]].neighbours)
 
 
 if __name__ == '__main__':
