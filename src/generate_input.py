09aca20fdc2d1e4454231e390f6e8470b41423c2
jsokolowska <joana.sokolowska@gmail.com>
2020-04-22 21:28:59 +0200

    Added graph generation function for ensuring a minimal nr of vertices on path

--- src/generate_input.py
+++ src/generate_input.py
@@ -10,8 +10,8 @@ from random import randrange
 
 
 def generate_input(vertex_num, probability, max_weight=20):
-    filename = "auto_graph_" + str(vertex_num) + "_" + str(probability) + ".txt"
-    generate_input_to_txt(vertex_num, probability, filename, max_weight)
+    file_name = "auto_graph_" + str(vertex_num) + "_" + str(probability) + ".txt"
+    generate_input_to_txt(vertex_num, probability, file_name, max_weight)
 
 
 def generate_input_to_txt(vertex_num, probability, file_name, max_weight=20):
@@ -99,3 +99,113 @@ def generate_input_to_txt(vertex_num, probability, file_name, max_weight=20):
         # # end_vertex = chr(ord('a') + randrange(0, vertex_num - 1))
         lines.append(" ".join([start_vertex, end_vertex]))
         file.write("\n".join(lines))
+
+
+def generate_with_bridges(vertex_num, probability, bridges_num, max_weight=20):
+    if vertex_num < 2 or probability < 0 or probability > 1 or bridges_num<1:
+        raise ValueError('Probability should be between 0 and 1, bridges_num greater than 1 while vertex_num must be at least 2')
+    if max_weight < 1:
+        raise ValueError("Argument max_weight must be at least 1.")
+    if vertex_num < bridges_num*2:
+        raise ValueError("Not enough vertices for that many bridges")
+    # generate vertices id's
+    all_vertices = []
+    max_letter_index = ord('z') - ord('a') + 1
+    i = 0
+    j = 0
+    while j < max_letter_index and i < vertex_num:
+        all_vertices.append(chr(ord("a") + j))
+        j += 1
+        i += 1
+
+    for k in range(vertex_num // max_letter_index):
+        j = 0
+        while j < max_letter_index and i < vertex_num:
+            all_vertices.append(all_vertices[k] + chr(ord("a") + j))
+            j += 1
+            i += 1
+
+    all_vertices = sorted(all_vertices)
+    # all_edges = [(all_vertices[a], all_vertices[b]) for a in range(vertex_num) for b in range(a + 1, vertex_num)]
+    groups = []
+    for bridge in range(bridges_num):
+        groups.append([])
+
+    # divide into as many groups as there are bridges
+    # for vertex in all_vertices:
+    #     groups[randrange(0, bridges_num)].append(vertex)
+
+    gr_size = vertex_num//bridges_num
+    for i in range(bridges_num-1):
+        groups[i] = all_vertices[i*gr_size:(i+1)*gr_size]
+
+    groups[bridges_num-1] = all_vertices[(bridges_num-1) * gr_size:]
+
+    global_start = groups[0][randrange(0, len(groups[0]))]
+    global_end = None
+
+    start_vertex = global_start
+    end_vertex = None
+    chosen_edges = set()
+    bridges_path = [start_vertex]
+    for i in range(len(groups)):
+        group = groups[i]
+        if len(group) == 0:
+            print("Empty group " + str(i) + " out of " + str(bridges_num))
+            continue
+        group_edges = [(group[a], group[b]) for a in range(len(group)) for b in range(a+1, len(group))]
+        group.remove(start_vertex)
+        end_vertex = group[randrange(0, len(group))]
+        used_edges = 0
+        curr_vertex = start_vertex
+        max_edges = len(group_edges)
+
+        # create path between them
+        while curr_vertex != end_vertex:
+            next_vertex = group[randrange(0, len(group))]
+            edge = (next_vertex, curr_vertex)
+            if curr_vertex < next_vertex:
+                edge = (curr_vertex, next_vertex)
+            group_edges.remove(edge)
+            chosen_edges.add(edge)
+            group.remove(next_vertex)
+            used_edges += 1
+            curr_vertex = next_vertex
+
+        bridges_path.append(end_vertex)
+        # create other paths in graph
+        if max_edges * probability > used_edges:
+            # adjust probability value to take into account used edges
+            probability = (probability * max_edges - used_edges) / (max_edges - used_edges)
+            for edge in group_edges:
+                rand_num = uniform(0, 1)
+                if rand_num < probability:
+                    chosen_edges.add(edge)
+
+        if i != len(groups)-1:
+            start_vertex = groups[i+1][randrange(0, len(groups[i+1]))]
+
+    global_end = end_vertex
+    # add all bridges:
+    if len(bridges_path) > 1:
+        for i in range(len(bridges_path)-1):
+            bridge = (bridges_path[i], bridges_path[i+1])
+            chosen_edges.add(bridge)
+
+    lines = []
+    for edge in chosen_edges:
+        weight = randrange(1, max_weight + 1)
+        lines.append(" ".join([edge[0], edge[1], str(weight)]))
+
+    path = "../graphs/"
+    file_name = "auto_graph_" + str(vertex_num) + "_" + str(probability) + ".txt"
+    file_name = path + file_name
+    with open(file_name, 'w') as file:
+        # start_vertex = all_vertices[randrange(0, vertex_num - 1)]
+        # # start_vertex = chr(ord('a') + randrange(0, vertex_num - 1))
+        # end_vertex = all_vertices[randrange(0, vertex_num - 1)]
+        # # end_vertex = chr(ord('a') + randrange(0, vertex_num - 1))
+        lines.append(" ".join([global_start, global_end]))
+        file.write("\n".join(lines))
+
+#generate_with_bridges(200, 0.5, 15)
--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
-import argparse
-
-
-parser = argparse.ArgumentParser(description="Ant Colony Optimization for finding a path in weighted graph")
-parser.add_argument('filename')
-# from src.ant_colony_optimization import AntColonyOptimization
-# from src.generate_input import generate_input
-# from src.graph_input import read_graph_txt
-# from src.path_search_algo import dijkstra
+# import argparse
 #
+#
+# parser = argparse.ArgumentParser(description="Ant Colony Optimization for finding a path in weighted graph")
+# parser.add_argument('filename')
+from src.ant_colony_optimization import AntColonyOptimization
+from src.generate_input import generate_input, generate_with_bridges
+from src.graph_input import read_graph_txt
+from src.path_search_algo import dijkstra
+
 # generate_input(100, 0.1, 30)
-# graph = read_graph_txt(keyword_1="auto", keyword_2="100")
-# # our_graph = read_graph_txt(keyword_1="graph", keyword_2="example")
-# dijk = dijkstra(graph, graph.start, graph.end)
+generate_with_bridges(200, 0.5, 15)
+graph = read_graph_txt(keyword_1="auto", keyword_2="20")
+# our_graph = read_graph_txt(keyword_1="graph", keyword_2="example")
+dijk = dijkstra(graph, graph.start, graph.end)
 # aco = AntColonyOptimization(ants_num=30, graph=graph)
 # result = aco.run(40)
 # print("Result: " + str(result))
-# print("Dijkstra: " + str(dijk))
+print("Dijkstra: " + str(dijk))
