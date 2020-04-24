5c39b9ec62fe19702398fdaffc261e790130026f
RafalUzarowicz <rafal.uzarowicz@gmail.com>
2020-04-24 22:36:21 +0200

    - added default value to iterations parameter in parser - added q parameter description

--- src/main.py
+++ src/main.py
@@ -20,14 +20,14 @@ parser.add_argument('-diff', action='store', type=float, default=0.5,
                          "the best solution by no more than given percent will be used for pheromone update, "
                          "must be between 0 and 1.")
 parser.add_argument('-q', action='store', type=float, default=1,
-                    help="")
+                    help="single ant pheromone multiplier")
 parser.add_argument('-ro', action='store', type=float, default=0.1,
                     help="decides how quickly pheromones on the graph edges fade, "
                          " must be between 0 and 1,"
                          " smaller value means slower fading, bigger means quicker fading")
 parser.add_argument('-a', '--ants_num', action='store', type=int, default=50,
                     help="number of ants used to generate solutions")
-parser.add_argument('-i', '--iterations', action='store', type=int,
+parser.add_argument('-i', '--iterations', action='store', type=int, default=1000,
                     help='max number of algorithm iterations.')
 args = parser.parse_args()
 
